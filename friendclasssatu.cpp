e5e6066fbe228fef5564976d12e4f1cf6bbf7e21
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-05-05 11:06:42 +0700

    membuat class dan modifiernya

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -2,13 +2,23 @@
 #include <string>
 using namespace std;
 
-class siswa ;
-class orang{
-    private :
+class siswa;
+class orang
+{
+private:
     string nama;
 
-    public :
+public:
     void setnama(string pnama);
     friend class siswa;
+};
 
+class siswa
+{
+private:
+    int id;
+
+public :
+    void setid(int pid );
+    void displayAll(orang &a);
 };
